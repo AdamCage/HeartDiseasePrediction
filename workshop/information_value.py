7978b468f372b317261177d5c848faa76874ef27
Bogdan Kishchak <yytrby@gmail.com>
2022-11-28 21:10:45 +0300

    Initial commit

---
+++ workshop/information_value.py
@@ -0,0 +1,137 @@
+import pandas as pd, numpy as np, os, re, math, time
+
+# to check monotonicity of a series
+def is_monotonic(temp_series):
+    return all(temp_series[i] <= temp_series[i + 1] for i in range(len(temp_series) - 1)) or all(temp_series[i] >= temp_series[i + 1] for i in range(len(temp_series) - 1))
+
+def prepare_bins(bin_data, c_i, target_col, max_bins):
+    force_bin = True
+    binned = False
+    remarks = np.nan
+    # ----------------- Monotonic binning -----------------
+    for n_bins in range(max_bins, 2, -1):
+        try:
+            bin_data[c_i + "_bins"] = pd.qcut(bin_data[c_i], n_bins, duplicates="drop")
+            monotonic_series = bin_data.groupby(c_i + "_bins")[target_col].mean().reset_index(drop=True)
+            if is_monotonic(monotonic_series):
+                force_bin = False
+                binned = True
+                remarks = "binned monotonically"
+                break
+        except:
+            pass
+    # ----------------- Force binning -----------------
+    # creating 2 bins forcefully because 2 bins will always be monotonic
+    if force_bin or (c_i + "_bins" in bin_data and bin_data[c_i + "_bins"].nunique() < 2):
+        _min=bin_data[c_i].min()
+        _mean=bin_data[c_i].mean()
+        _max=bin_data[c_i].max()
+        bin_data[c_i + "_bins"] = pd.cut(bin_data[c_i], [_min, _mean, _max], include_lowest=True)
+        if bin_data[c_i + "_bins"].nunique() == 2:
+            binned = True
+            remarks = "binned forcefully"
+
+    if binned:
+        return c_i + "_bins", remarks, bin_data[[c_i, c_i+"_bins", target_col]].copy()
+    else:
+        remarks = "couldn't bin"
+        return c_i, remarks, bin_data[[c_i, target_col]].copy()
+
+# calculate WOE and IV for every group/bin/class for a provided feature
+def iv_woe_4iter(binned_data, target_col, class_col):
+    if "_bins" in class_col:
+        binned_data[class_col] = binned_data[class_col].cat.add_categories(['Missing'])
+        binned_data[class_col] = binned_data[class_col].fillna("Missing")
+        temp_groupby = binned_data.groupby(class_col).agg({class_col.replace("_bins", ""):["min", "max"],
+                                                           target_col: ["count", "sum", "mean"]}).reset_index()
+    else:
+        binned_data[class_col] = binned_data[class_col].fillna("Missing")
+        temp_groupby = binned_data.groupby(class_col).agg({class_col:["first", "first"],
+                                                           target_col: ["count", "sum", "mean"]}).reset_index()
+
+    temp_groupby.columns = ["sample_class", "min_value", "max_value", "sample_count", "event_count", "event_rate"]
+    temp_groupby["non_event_count"] = temp_groupby["sample_count"] - temp_groupby["event_count"]
+    temp_groupby["non_event_rate"] = 1 - temp_groupby["event_rate"]
+    temp_groupby = temp_groupby[["sample_class", "min_value", "max_value", "sample_count",
+                                 "non_event_count", "non_event_rate", "event_count", "event_rate"]]
+
+    if "_bins" not in class_col and "Missing" in temp_groupby["min_value"]:
+        temp_groupby["min_value"] = temp_groupby["min_value"].replace({"Missing": np.nan})
+        temp_groupby["max_value"] = temp_groupby["max_value"].replace({"Missing": np.nan})
+    temp_groupby["feature"] = class_col
+    if "_bins" in class_col:
+        temp_groupby["sample_class_label"]=temp_groupby["sample_class"].replace({"Missing": np.nan}).astype('category').cat.codes.replace({-1: np.nan})
+    else:
+        temp_groupby["sample_class_label"]=np.nan
+    temp_groupby = temp_groupby[["feature", "sample_class", "sample_class_label", "sample_count", "min_value", "max_value",
+                                 "non_event_count", "non_event_rate", "event_count", "event_rate"]]
+
+    """
+    **********get distribution of good and bad
+    """
+    temp_groupby['distbn_non_event'] = temp_groupby["non_event_count"]/temp_groupby["non_event_count"].sum()
+    temp_groupby['distbn_event'] = temp_groupby["event_count"]/temp_groupby["event_count"].sum()
+
+    temp_groupby['woe'] = np.log(temp_groupby['distbn_non_event'] / temp_groupby['distbn_event'])
+    temp_groupby['iv'] = (temp_groupby['distbn_non_event'] - temp_groupby['distbn_event']) * temp_groupby['woe']
+
+    temp_groupby["woe"] = temp_groupby["woe"].replace([np.inf,-np.inf],0)
+    temp_groupby["iv"] = temp_groupby["iv"].replace([np.inf,-np.inf],0)
+
+    return temp_groupby
+
+"""
+- iterate over all features.
+- calculate WOE & IV for there classes.
+- append to one DataFrame woe_iv.
+"""
+def var_iter(data, target_col, max_bins):
+    woe_iv = pd.DataFrame()
+    remarks_list = []
+    for c_i in data.columns:
+        if c_i not in [target_col]:
+            # check if binning is required. if yes, then prepare bins and calculate woe and iv.
+            """
+            ----logic---
+            binning is done only when feature is continuous and non-binary.
+            Note: Make sure dtype of continuous columns in dataframe is not object.
+            """
+            c_i_start_time=time.time()
+            if np.issubdtype(data[c_i], np.number) and data[c_i].nunique() > 2:
+                class_col, remarks, binned_data = prepare_bins(data[[c_i, target_col]].copy(), c_i, target_col, max_bins)
+                agg_data = iv_woe_4iter(binned_data.copy(), target_col, class_col)
+                remarks_list.append({"feature": c_i, "remarks": remarks})
+            else:
+                agg_data = iv_woe_4iter(data[[c_i, target_col]].copy(), target_col, c_i)
+                remarks_list.append({"feature": c_i, "remarks": "categorical"})
+            # print("---{} seconds. c_i: {}----".format(round(time.time() - c_i_start_time, 2), c_i))
+            woe_iv = woe_iv.append(agg_data)
+    return woe_iv, pd.DataFrame(remarks_list)
+
+# after getting woe and iv for all classes of features calculate aggregated IV values for features.
+def get_iv_woe(data, target_col, max_bins):
+    func_start_time = time.time()
+    woe_iv, binning_remarks = var_iter(data, target_col, max_bins)
+    print("------------------IV and WOE calculated for individual groups.------------------")
+    print("Total time elapsed: {} minutes".format(round((time.time() - func_start_time) / 60, 3)))
+
+    woe_iv["feature"] = woe_iv["feature"].replace("_bins", "", regex=True)
+    woe_iv = woe_iv[["feature", "sample_class", "sample_class_label", "sample_count", "min_value", "max_value",
+                     "non_event_count", "non_event_rate", "event_count", "event_rate", 'distbn_non_event',
+                     'distbn_event', 'woe', 'iv']]
+
+    iv = woe_iv.groupby("feature")[["iv"]].agg(["sum", "count"]).reset_index()
+    print("------------------Aggregated IV values for features calculated.------------------")
+    print("Total time elapsed: {} minutes".format(round((time.time() - func_start_time) / 60, 3)))
+
+    iv.columns = ["feature", "iv", "number_of_classes"]
+    null_percent_data=pd.DataFrame(data.isnull().mean()).reset_index()
+    null_percent_data.columns=["feature", "feature_null_percent"]
+    iv=iv.merge(null_percent_data, on="feature", how="left")
+    print("------------------Null percent calculated in features.------------------")
+    print("Total time elapsed: {} minutes".format(round((time.time() - func_start_time) / 60, 3)))
+    iv = iv.merge(binning_remarks, on="feature", how="left")
+    woe_iv = woe_iv.merge(iv[["feature", "iv", "remarks"]].rename(columns={"iv": "iv_sum"}), on="feature", how="left")
+    print("------------------Binning remarks added and process is complete.------------------")
+    print("Total time elapsed: {} minutes".format(round((time.time() - func_start_time) / 60, 3)))
+    return iv, woe_iv.replace({"Missing": np.nan})
